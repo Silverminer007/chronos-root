d7412e43618b4c358fee7126128ed9b2f2f3b643
justus <henze5@gmx.de>
2025-12-08 11:44:22 +0100

    change primary and background color

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,25 @@
 import tailwindcss from "@tailwindcss/vite";
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
+
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -33,11 +53,11 @@ export default defineNuxtConfig({
     primevue: {
         options: {
             theme: {
-                preset: Aura
+                preset: MyPreset
             }
         },
         directives: {
             include: ['Tooltip']
-        }
+        },
     }
 })
