96a63db415a82d0d103adde7823bf72c10aaae35
justus <henze5@gmx.de>
2025-12-09 11:41:38 +0100

    feat: allow fetching attendances of single events

--- backend/src/main/java/de/chronos_live/chronos_date_api/presentation/EventResource.java
+++ backend/src/main/java/de/chronos_live/chronos_date_api/presentation/EventResource.java
@@ -1,11 +1,14 @@
 package de.chronos_live.chronos_date_api.presentation;
 
+import de.chronos_live.chronos_date_api.application.AttendanceStatusService;
 import de.chronos_live.chronos_date_api.application.EventAccessService;
 import de.chronos_live.chronos_date_api.application.EventService;
 import de.chronos_live.chronos_date_api.application.UserService;
+import de.chronos_live.chronos_date_api.domain.Attendance;
 import de.chronos_live.chronos_date_api.domain.Event;
 import de.chronos_live.chronos_date_api.domain.EventAttendeeRole;
 import de.chronos_live.chronos_date_api.domain.User;
+import de.chronos_live.chronos_date_api.mapper.AttendanceMapper;
 import de.chronos_live.chronos_date_api.mapper.EventMapper;
 import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
@@ -15,6 +18,8 @@ import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
+import java.util.List;
+
 @Path("/api/v2/event")
 @PermitAll
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,22 +28,27 @@ public class EventResource {
     private final EventService eventService;
     private final EventAccessService eventAccessService;
     private final UserService userService;
+    private final AttendanceStatusService attendanceStatusService;
 
     @Inject
     JsonWebToken jwt;
 
     @Inject
-    EventMapper mapper;
+    EventMapper eventMapper;
 
-    public EventResource(EventService eventService, EventAccessService eventAccessService, UserService userService) {
+    @Inject
+    AttendanceMapper attendanceMapper;
+
+    public EventResource(EventService eventService, EventAccessService eventAccessService, UserService userService, AttendanceStatusService attendanceStatusService) {
         this.eventService = eventService;
         this.eventAccessService = eventAccessService;
         this.userService = userService;
+        this.attendanceStatusService = attendanceStatusService;
     }
 
     @GET
     @Path("/{id}")
-    public Response getEvent(@PathParam("id") int id) {
+    public Response getEvent(@PathParam("id") int id, @QueryParam("attendances") Boolean includeAttendances) {
         User user = userService.getUser(jwt.getSubject());
 
         Event event = eventService.getEvent(id);
@@ -50,7 +60,17 @@ public class EventResource {
         if (!eventAccessService.userHasAccessToEvent(user, event)) {
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
-        return Response.ok(mapper.toDto(event)).build();
+        List<Attendance> attendances = this.attendanceStatusService.getAttendanceStatus(event.id);
+        Attendance ownAttendance = this.attendanceStatusService.getAttendanceStatus(user, event.id);
+
+        if(includeAttendances != null && includeAttendances) {
+            EventDto eventDto = eventMapper.toDto(event)
+                    .withOwnAttendanceStatus(ownAttendance.getStatus().name())
+                    .withAttendances(attendanceMapper.toDtoList(attendances));
+            return Response.ok(eventDto).build();
+        } else {
+            return Response.ok(eventMapper.toDto(event)).build();
+        }
     }
 
     @POST
@@ -58,7 +78,7 @@ public class EventResource {
     public Response postEvent(@RequestBody EventDto eventDto) {
         User user = userService.getUser(jwt.getSubject());
 
-        Event event = mapper.toEntity(eventDto);
+        Event event = eventMapper.toEntity(eventDto);
         try {
             this.eventService.createEvent(event);
             this.eventAccessService.assignUserToEvent(user, event.id, user.id, EventAttendeeRole.RESPONSIBLE, true);
@@ -66,7 +86,7 @@ public class EventResource {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         }
 
-        return Response.status(Response.Status.CREATED).entity(mapper.toDto(event)).build();
+        return Response.status(Response.Status.CREATED).entity(eventMapper.toDto(event)).build();
     }
 
     @PATCH
@@ -74,7 +94,7 @@ public class EventResource {
     public Response patchEvent(@RequestBody EventDto eventDto, @PathParam("id") Long id) {
         User user = userService.getUser(jwt.getSubject());
 
-        Event event = mapper.toEntity(eventDto);
+        Event event = eventMapper.toEntity(eventDto);
         event.id = id;
 
         if (!eventAccessService.userHasAccessToEvent(user, event)) {
@@ -87,7 +107,7 @@ public class EventResource {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
         }
 
-        return Response.ok(mapper.toDto(event)).build();
+        return Response.ok(eventMapper.toDto(event)).build();
     }
 
     @DELETE
