25dcfd4caf94c7d63775bae73bfd5f6dc440bbec
justus <henze5@gmx.de>
2025-12-09 10:41:58 +0100

    add datecard skeletons for loading state

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import ProgressSpinner from 'primevue/progressspinner';
 import {useAuthStore} from "~/stores/auth";
 
 definePageMeta({
@@ -16,12 +15,9 @@ callOnce('events', () => eventStore.fetchEvents())
   <SearchHeader/>
   <!-- Body -->
   <div class="p-4 pt-20 flex flex-col gap-4">
-    <p v-if="eventStore.error" class="text-red-500 text-sm"><span class="pi pi-exclamation-triangle"/> {{eventStore.error}}</p>
-    <p v-if="eventStore.loading" class="text-blue-500">
-      Loading events
-    </p>
-    <ProgressSpinner v-if="eventStore.loading"/>
     <DateCard v-for="event in eventStore.events" :key="event.id" :event="event" />
+    <p v-if="eventStore.error" class="text-red-500 text-sm"><span class="pi pi-exclamation-triangle"/> {{eventStore.error}}</p>
+    <DateCardSkeleton v-if="eventStore.loading" v-for="i in [1, 2, 3, 4, 5]" :key="i"/>
     <div class="flex flex-row w-full items-center justify-center">
       <Button @click="eventStore.fetchEvents">
         Mehr laden
