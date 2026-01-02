bedaa7c0d667f4b8b33b8a053b8ee74ad0bdd54a
justus <henze5@gmx.de>
2026-01-02 18:12:48 +0100

    install sentry

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,62 +3,76 @@ import {ChronosTheme} from "./theme";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
-    runtimeConfig: {
-        auth: {
-            issuer: "",          // wird per ENV überschrieben
-            clientId: "",
-            redirectUri: "",
-        },
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true},
 
-        // Optional: API Endpoint zu Quarkus
-        quarkusUrl: ""
-    },
-    devServer: {
-        port: 3000,
-        host: '0.0.0.0' // do not put localhost (only accessible from the host machine)
-    },
-    css: ["./app/assets/css/main.css",],
-    vite: {
-        plugins: [
-            tailwindcss(),
-        ],
-    },
-    modules: [
-        '@primevue/nuxt-module',
-        '@pinia/nuxt'
-    ],
-    primevue: {
-        options: {
-            theme: {
-                preset: ChronosTheme
-            }
-        },
-        directives: {
-            include: ['Tooltip']
-        },
-    },
-    app: {
-        head: {
-            link: [
-                {
-                    rel: 'manifest',
-                    href: '/manifest.webmanifest'
-                },
-                {
-                    rel: 'icon',
-                    href: '/icons/icon-192.png',
-                    sizes: '192x192',
-                    type: 'image/png'
-                },
-                {
-                    rel: 'icon',
-                    href: '/icons/icon-512.png',
-                    sizes: '512x512',
-                    type: 'image/png'
-                }
-            ]
-        }
-    }
+  runtimeConfig: {
+      auth: {
+          issuer: "",          // wird per ENV überschrieben
+          clientId: "",
+          redirectUri: "",
+      },
+
+      // Optional: API Endpoint zu Quarkus
+      quarkusUrl: ""
+  },
+
+  devServer: {
+      port: 3000,
+      host: '0.0.0.0' // do not put localhost (only accessible from the host machine)
+  },
+
+  css: ["./app/assets/css/main.css",],
+
+  vite: {
+      plugins: [
+          tailwindcss(),
+      ],
+  },
+
+  modules: ['@primevue/nuxt-module', '@pinia/nuxt', '@sentry/nuxt/module'],
+
+  primevue: {
+      options: {
+          theme: {
+              preset: ChronosTheme
+          }
+      },
+      directives: {
+          include: ['Tooltip']
+      },
+  },
+
+  app: {
+      head: {
+          link: [
+              {
+                  rel: 'manifest',
+                  href: '/manifest.webmanifest'
+              },
+              {
+                  rel: 'icon',
+                  href: '/icons/icon-192.png',
+                  sizes: '192x192',
+                  type: 'image/png'
+              },
+              {
+                  rel: 'icon',
+                  href: '/icons/icon-512.png',
+                  sizes: '512x512',
+                  type: 'image/png'
+              }
+          ]
+      }
+  },
+
+  sentry: {
+    org: 'justus-henze',
+    project: 'chronos-nuxt',
+    autoInjectServerSentry: 'top-level-import',
+  },
+
+  sourcemap: {
+    client: 'hidden',
+  },
 })
