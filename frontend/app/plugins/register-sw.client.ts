25e50634bcda587876e42ae23f6a6ab76d5d02c3
justus <henze5@gmx.de>
2026-02-03 14:40:58 +0100

    feat: check for sw updates every 60 minutes

--- frontend/app/plugins/register-sw.client.ts
+++ frontend/app/plugins/register-sw.client.ts
@@ -4,7 +4,12 @@ export default defineNuxtPlugin(() => {
     });
 
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/push-sw.js").catch(err => {
+        navigator.serviceWorker.register("/push-sw.js").then(registration => {
+            // Check for updates every 60 minutes
+            setInterval(() => {
+                registration.update();
+            }, 60 * 60 * 1000);
+        }).catch(err => {
             console.error("Service worker registration failed", err);
         });
     }
